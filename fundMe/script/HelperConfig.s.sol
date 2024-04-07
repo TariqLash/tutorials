f186887a8f0ff413baea1349c48a3e3efe80e35e
Tariq Lashley <tariqlash@gmail.com>
2024-04-07 13:02:59 -0600

    added a bunch of tests

--- fundMe/script/HelperConfig.s.sol
+++ fundMe/script/HelperConfig.s.sol
@@ -8,10 +8,14 @@ pragma solidity ^0.8.18;
 // Mainnet ETH/USD
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script{
     
     NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig{
         address priceFeed; // ETH/USD price feed address
@@ -23,7 +27,7 @@ contract HelperConfig {
         } else if(block.chainid == 1){
             activeNetworkConfig = getMainnetEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -43,7 +47,25 @@ contract HelperConfig {
         return ethConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory){
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory){
 
+        if(activeNetworkConfig.priceFeed != address(0)){
+            return activeNetworkConfig;
+        }
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS, 
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
     }
 }
